e30824c0ca3967a08bff947cf21fa7760b9a6eb1
Sakhavat <sakhavatra@code.edu.az>
2022-03-16 10:21:53 +0400

    first commit

---
+++ FirstAPP/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstAPP.Controllers
+{
+    
+    public class HomeController :Controller
+    {
+        
+        public IActionResult Index( int a)
+        {
+            ViewData["Name"] = "Vetendas";
+            ViewData["Surname"] = "Aliyev";
+
+            ViewBag.Name1 = "Vetendas";
+            ViewBag.Surname1 = "Veliyev";
+
+            TempData["String"] = "Butun Huqulari qorunur";
+                return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
